3e40482e052c2297c76287c1aa0480869aad7578
Thibaut Izard <thibaut.izard@gmail.com>
2025-09-12 13:03:59 +0200

    feat(app): Start homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,89 @@
-import Image from "next/image";
+"use client";
+import { motion } from "motion/react";
+import {
+  ArrowRightIcon,
+  ChevronsLeftRightIcon,
+  UsersRoundIcon,
+  ZapIcon,
+  type LucideProps,
+} from "lucide-react";
+import Subtitle from "./components/subtitle";
+import SudWeb from "./components/sud-web";
+import { Button } from "@/components/ui/button";
+
+function Item({
+  title,
+  subtitle,
+  Icon,
+}: {
+  title: string;
+  subtitle: string;
+  Icon: React.ComponentType<LucideProps>;
+}) {
+  return (
+    <div className="flex flex-col items-center gap-1">
+      <h3 className="text-3xl font-bold flex items-center gap-2">
+        <Icon size={34} className="text-purple-600" strokeWidth={2} />
+        <span className="font-black">{title}</span>
+      </h3>
+      <div className="text-base text-gray-500">{subtitle}</div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-screen w-full grid place-items-center">
+      <div className="flex flex-col items-center gap-2">
+        <SudWeb />
+        <Subtitle>
+          Nous créons des sites web et applications sur mesure pour propulser
+          votre entreprise vers le succès digital. Expertise technique, design
+          moderne, résultats garantis.
+        </Subtitle>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="mt-4 flex gap-4"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Button className="rounded-full hover:bg-purple-600/90 hover:cursor-pointer hover:scale-101 bg-gradient-to-r from-purple-600 to-purple-400 px-8 py-5.5 text-lg font-bold">
+            <div className="flex gap-3 items-center">
+              <span>Démarrer un projet</span>
+              <ArrowRightIcon className="w-4 h-4" />
+            </div>
+          </Button>
+          <Button className="rounded-full bg-black border-2 hover:cursor-pointer border-purple-600 px-8 py-5.5 text-lg font-bold">
+            <div className="flex gap-3 items-center">
+              <span>Voir nos réalisations</span>
+            </div>
+          </Button>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50, scale: 0.8 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{
+            duration: 0.6,
+            delay: 0.7,
+            type: "spring",
+            stiffness: 200,
+            damping: 15,
+          }}
+          className="flex gap-12 mt-6"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <Item
+            title="100+"
+            subtitle="Projets réalisés"
+            Icon={ChevronsLeftRightIcon}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <Item
+            title="50+"
+            subtitle="Clients satisfaits"
+            Icon={UsersRoundIcon}
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <Item title="10+" subtitle="Années d'expérience" Icon={ZapIcon} />
+        </motion.div>
+      </div>
     </div>
   );
 }
