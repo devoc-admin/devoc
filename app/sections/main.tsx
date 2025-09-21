df931ca822a2716a486ff88f00b67e9295c88805
Thibaut Izard <thibaut.izard@gmail.com>
2025-09-21 18:28:08 +0200

    ✨ feat: Add badge to main page

--- app/sections/main.tsx
+++ app/sections/main.tsx
@@ -3,6 +3,7 @@ import {
   ArrowRightIcon,
   ChevronsLeftRightIcon,
   type LucideProps,
+  Sparkles,
   UsersRoundIcon,
   ZapIcon,
 } from "lucide-react";
@@ -11,39 +12,34 @@ import { Button } from "@/components/ui/button";
 import Subtitle from "../components/subtitle";
 import SudWeb from "../components/sud-web";
 
-function Item({
-  title,
-  subtitle,
-  Icon,
-}: {
-  title: string;
-  subtitle: string;
-  Icon: React.ComponentType<LucideProps>;
-}) {
-  return (
-    <div className="flex flex-col items-center gap-1">
-      <h3 className="text-3xl font-bold flex items-center gap-2">
-        <Icon size={34} className="text-purple-600" strokeWidth={2} />
-        <span className="font-black">{title}</span>
-      </h3>
-      <div className="text-base text-gray-500">{subtitle}</div>
-    </div>
-  );
-}
+const ANIMATION_DELAY = 0.5;
 
 export default function Main() {
   return (
     <div className="flex h-screen w-full flex-col justify-center items-center gap-2">
+      {/* 🟪 Badge */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: ANIMATION_DELAY * 2 }}
+        className="flex font-semibold items-center gap-2 text-sm bg-purple-100 rounded-full px-4 py-2"
+      >
+        <Sparkles size={16} className="text-purple-600" />
+        <span className="text-purple-600">Votre agence web innovante</span>
+      </motion.div>
+      {/* 🆎 Title */}
       <SudWeb />
+      {/* 🔤 Subtitle */}
       <Subtitle>
         Nous créons des sites web et applications sur mesure pour propulser
         votre organisation vers le succès digital. Expertise technique, design
         moderne, résultats garantis.
       </Subtitle>
+      {/* 🔤 Buttons */}
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.5 }}
+        transition={{ duration: 0.5, delay: ANIMATION_DELAY }}
         className="mt-4 flex gap-4"
       >
         <Button className="rounded-full hover:bg-purple-600/90 hover:cursor-pointer hover:scale-103 bg-gradient-to-r from-purple-600 to-purple-400 px-8 py-5.5 text-lg font-bold">
@@ -63,7 +59,7 @@ export default function Main() {
         animate={{ opacity: 1, y: 0, scale: 1 }}
         transition={{
           duration: 0.6,
-          delay: 0.7,
+          delay: ANIMATION_DELAY + 0.2,
           type: "spring",
           stiffness: 200,
           damping: 15,
@@ -78,6 +74,26 @@ export default function Main() {
         <Item title="50+" subtitle="Clients satisfaits" Icon={UsersRoundIcon} />
         <Item title="10+" subtitle="Années d'expérience" Icon={ZapIcon} />
       </motion.div>
+    </div>
+  );
+}
+
+function Item({
+  title,
+  subtitle,
+  Icon,
+}: {
+  title: string;
+  subtitle: string;
+  Icon: React.ComponentType<LucideProps>;
+}) {
+  return (
+    <div className="flex flex-col items-center gap-1">
+      <h3 className="text-3xl font-bold flex items-center gap-2">
+        <Icon size={34} className="text-purple-600" strokeWidth={2} />
+        <span className="font-black">{title}</span>
+      </h3>
+      <div className="text-base text-gray-500">{subtitle}</div>
     </div>
   );
 }
