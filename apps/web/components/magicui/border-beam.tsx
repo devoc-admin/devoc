48b2e03755e3987d3ee4459bd16ca090b2942941
Thibaut Izard <thibaut.izard@gmail.com>
2025-11-30 19:53:44 +0100

    perf(web): improve service card

--- apps/web/components/magicui/border-beam.tsx
+++ apps/web/components/magicui/border-beam.tsx
@@ -4,7 +4,66 @@ import { motion, MotionStyle, Transition } from "motion/react"
 
 import { cn } from "@/lib/utils"
 
-interface BorderBeamProps {
+
+export function BorderBeam({
+  className,
+  size = 50,
+  delay = 0,
+  duration = 6,
+  colorFrom = "var(--primary)",
+  colorTo = "var(--primary)",
+  transition,
+  style,
+  reverse = false,
+  initialOffset = 0,
+  borderWidth = 1,
+}: BorderBeamProps) {
+  return (
+    <div
+      className="pointer-events-none rounded-lg absolute inset-0 border-(length:--border-beam-width) border-transparent mask-[linear-gradient(transparent,transparent),linear-gradient(#000,#000)] mask-intersect [mask-clip:padding-box,border-box]"
+      style={
+        {
+          "--border-beam-width": `${borderWidth}px`,
+        } as React.CSSProperties
+      }
+    >
+      <motion.div
+        className={cn(
+          "absolute aspect-square",
+          "bg-linear-to-l",
+          "from-(--color-from) via-(--color-to) to-transparent",
+          reverse && "from-transparent via-(--color-to) to-(--color-from)",
+          className
+        )}
+        style={
+          {
+            width: size,
+            offsetPath: `rect(0 auto auto 0 round ${size}px)`,
+            "--color-from": colorFrom,
+            "--color-to": colorTo,
+            ...style,
+          } as MotionStyle
+        }
+        initial={{ offsetDistance: `${initialOffset}%` }}
+        animate={{
+          offsetDistance: reverse
+            ? [`${100 - initialOffset}%`, `${-initialOffset}%`]
+            : [`${initialOffset}%`, `${100 + initialOffset}%`],
+        }}
+        transition={{
+          repeat: Infinity,
+          ease: "linear",
+          duration,
+          delay: -delay,
+          ...transition,
+        }}
+      />
+    </div>
+  )
+}
+
+
+type BorderBeamProps = {
   /**
    * The size of the border beam.
    */
@@ -49,62 +108,4 @@ interface BorderBeamProps {
    * The border width of the beam.
    */
   borderWidth?: number
-}
-
-export const BorderBeam = ({
-  className,
-  size = 50,
-  delay = 0,
-  duration = 6,
-  colorFrom = "var(--primary)",
-  colorTo = "var(--primary)",
-  transition,
-  style,
-  reverse = false,
-  initialOffset = 0,
-  borderWidth = 1,
-}: BorderBeamProps) => {
-  return (
-    <div
-      className="pointer-events-none rounded-lg absolute inset-0 border-(length:--border-beam-width) border-transparent mask-[linear-gradient(transparent,transparent),linear-gradient(#000,#000)] mask-intersect [mask-clip:padding-box,border-box]"
-      style={
-        {
-          "--border-beam-width": `${borderWidth}px`,
-        } as React.CSSProperties
-      }
-    >
-      <motion.div
-        className={cn(
-          "absolute aspect-square",
-          "bg-linear-to-l",
-          "from-(--color-from) via-(--color-to) to-transparent",
-          reverse && "from-transparent via-(--color-to) to-(--color-from)",
-          className
-        )}
-        style={
-          {
-            width: size,
-            offsetPath: `rect(0 auto auto 0 round ${size}px)`,
-            "--color-from": colorFrom,
-            "--color-to": colorTo,
-            ...style,
-          } as MotionStyle
-        }
-        initial={{ offsetDistance: `${initialOffset}%` }}
-        animate={{
-          offsetDistance: reverse
-            ? [`${100 - initialOffset}%`, `${-initialOffset}%`]
-            : [`${initialOffset}%`, `${100 + initialOffset}%`],
-        }}
-        transition={{
-          repeat: Infinity,
-          ease: "linear",
-          duration,
-          delay: -delay,
-          ...transition,
-        }}
-      />
-    </div>
-  )
-
 }
