706599c96e0c63ffc4fb98b1cd7f3a97549b8404
Thibaut Izard <thibaut.izard@gmail.com>
2026-01-24 15:33:40 +0100

    fix(web): fix method images public path

--- apps/web/app/_components/sections/method/method-type.ts
+++ apps/web/app/_components/sections/method/method-type.ts
@@ -8,37 +8,37 @@ export const steps: Step[] = [
   {
     description:
       "Après un premier contact, nous nous rencontrons pour échanger sur vos besoins, vos délais et votre budget.",
-    image: "/method/meet.avif",
+    image: "/images/method/meet.avif",
     title: "Rencontre",
   },
   {
     description:
       "Nous élaborons une solution sur-mesure à partir d'un cahier des charges.",
-    image: "/method/plan.avif",
+    image: "/images/method/plan.avif",
     title: "Proposition",
   },
   {
     description:
       "Une fois le plan validé, nous lançons la réalisation de votre projet !",
-    image: "/method/handshake.avif",
+    image: "/images/method/handshake.avif",
     title: "Validation",
   },
   {
     description:
       "Durant le développement, vous restez informé de l'avancement de votre projet et vous recevez vos livrables selon le calendrier défini.",
-    image: "/method/machine.avif",
+    image: "/images/method/machine.avif",
     title: "Production",
   },
   {
     description:
       "Nous vous formons pour vous approprier vos outils afin de devenir complètement autonomes.",
-    image: "/method/light_bulb.avif",
+    image: "/images/method/light_bulb.avif",
     title: "Formation",
   },
   {
     description:
       "Nous nous occupons de la maintenance pour que votre projet fonctionne sans interruption et en toute sécurité !",
-    image: "/method/deck_chair.avif",
+    image: "/images/method/deck_chair.avif",
     title: "Maintenance",
   },
 ];
