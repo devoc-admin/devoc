da2ba3f7c851be6631a3866eeedee9473f5a4497
Thibaut Izard <thibaut.izard@gmail.com>
2026-01-24 16:35:50 +0100

    fix(web): fix method animations

--- apps/web/app/_components/sections/method/components/method-desktop.tsx
+++ apps/web/app/_components/sections/method/components/method-desktop.tsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
 import { type Step, steps } from "../method-type";
-
 export function MethodDesktop() {
   return (
     <div className="relative w-full space-y-50 overflow-x-hidden py-36">
@@ -13,18 +13,27 @@ export function MethodDesktop() {
 }
 
 // ------------------------------------------------
-
+// 🃏 Card
 function CardStep({
   title,
   description,
   image,
   index,
 }: Step & { index: number }) {
-  const fromLeft = index % 2 === 0;
+  const fromLeft = index % 2 !== 0;
+
   return (
     <div className="relative">
+      {/* 🖼️🔢 Image + Number */}
       <MoveRotateFromOutside fromLeft={fromLeft}>
-        <div className="size-100 rounded-xl border-6 border-primary p-4">
+        <div
+          className={cn(
+            "size-100",
+            "rounded-xl",
+            "border-6 border-primary",
+            "p-4"
+          )}
+        >
           <Image
             alt={title}
             className="max-w-full"
@@ -32,18 +41,69 @@ function CardStep({
             src={image}
             width={500}
           />
-          <div className="absolute bottom-0 left-1/2 flex size-16 -translate-x-1/2 translate-y-1/2 items-center justify-center rounded-full bg-primary p-3 font-bold font-kanit text-4xl text-white">
-            {index + 1}
+          <div
+            className={cn(
+              "flex items-center justify-center",
+              "absolute",
+              "bottom-0 left-1/2",
+              "-translate-x-1/2 translate-y-1/2",
+              "size-16",
+              "p-3",
+              "rounded-full",
+              "bg-primary",
+              "font-bold font-kanit text-4xl text-white"
+            )}
+          >
+            {/* 🔢 Number */}
+            <span>{index + 1}</span>
           </div>
         </div>
       </MoveRotateFromOutside>
+      {/* 🔠 Text */}
       <FadeMoveFromOutside fromLeft={fromLeft}>
-        <div className="flex max-w-100 flex-col gap-y-2 p-4 font-kanit">
-          <h3 className="font-bold text-7xl">{title}</h3>
+        <div className={cn("flex flex-col gap-y-2", "max-w-110", "p-4")}>
+          <h3 className="font-bold font-kanit text-7xl">{title}</h3>
           <div className="text-xl leading-tight">{description}</div>
         </div>
       </FadeMoveFromOutside>
     </div>
+  );
+}
+
+// --------------------------------
+// 🤹 Animations
+function MoveRotateFromOutside({
+  children,
+  fromLeft,
+}: {
+  children: React.ReactNode;
+  fromLeft: boolean;
+}) {
+  const rotationIntensity = Math.round(window.innerWidth / 15);
+
+  return (
+    <motion.div
+      className={cn("relative", "left-1/2", "w-fit")}
+      initial={{
+        opacity: 0,
+        rotate: fromLeft ? rotationIntensity : -rotationIntensity,
+        x: fromLeft ? "min(50vw, 1000px)" : "max(-50vw, -1000px)",
+      }}
+      transition={{
+        bounce: 0,
+        duration: 2.5,
+        ease: "easeOut",
+        type: "spring",
+      }}
+      viewport={{ margin: "-200px 0px 0px 0px", once: true }}
+      whileInView={{
+        opacity: 1,
+        rotate: fromLeft ? 7 : -7,
+        x: fromLeft ? "15%" : "-100%",
+      }}
+    >
+      {children}
+    </motion.div>
   );
 }
 
@@ -54,103 +114,23 @@ function FadeMoveFromOutside({
   children: React.ReactNode;
   fromLeft: boolean;
 }) {
-  if (fromLeft) {
-    return (
-      <motion.div
-        className="absolute top-1/2 w-fit translate-x-[25%] -translate-y-1/2"
-        initial={{
-          left: "80%",
-          opacity: 0,
-        }}
-        transition={{
-          bounce: 0,
-          duration: 2.5,
-          ease: "easeOut",
-          type: "spring",
-        }}
-        viewport={{ margin: "-20px", once: true }}
-        whileInView={{ left: "50%", opacity: 1 }}
-      >
-        {children}
-      </motion.div>
-    );
-  }
-
-  if (!fromLeft) {
-    return (
-      <motion.div
-        className="absolute top-1/2 w-fit translate-x-[25%] -translate-y-1/2"
-        initial={{
-          left: "0%",
-          opacity: 0,
-        }}
-        transition={{
-          bounce: 0,
-          duration: 2.5,
-          ease: "easeOut",
-          type: "spring",
-        }}
-        viewport={{ margin: "-20px", once: true }}
-        whileInView={{ left: "10%", opacity: 1 }}
-      >
-        {children}
-      </motion.div>
-    );
-  }
-}
-
-function MoveRotateFromOutside({
-  children,
-  fromLeft,
-}: {
-  children: React.ReactNode;
-  fromLeft: boolean;
-}) {
-  if (fromLeft) {
-    return (
-      <motion.div
-        className="relative w-fit -translate-x-[125%]"
-        initial={{
-          left: 70,
-          opacity: 1,
-          rotate: 200,
-          y: 150,
-        }}
-        transition={{
-          bounce: 0,
-          duration: 2.5,
-          ease: "easeOut",
-          type: "spring",
-        }}
-        viewport={{ margin: "0px 0px 0px 0px", once: true }}
-        whileInView={{ left: "50%", opacity: 1, rotate: -10, y: 0 }}
-      >
-        {children}
-      </motion.div>
-    );
-  }
-
-  if (!fromLeft) {
-    return (
-      <motion.div
-        className="relative w-fit translate-x-[25%]"
-        initial={{
-          left: "calc(100% - 70px)",
-          opacity: 0,
-          rotate: 200,
-          y: -150,
-        }}
-        transition={{
-          bounce: 0,
-          duration: 2.5,
-          ease: "easeOut",
-          type: "spring",
-        }}
-        viewport={{ margin: "-20px", once: true }}
-        whileInView={{ left: "50%", opacity: 1, rotate: 10, y: 0 }}
-      >
-        {children}
-      </motion.div>
-    );
-  }
+  return (
+    <motion.div
+      className={cn("absolute", "top-1/2 left-1/2 -translate-y-1/2", "w-fit")}
+      initial={{
+        opacity: 0,
+        x: fromLeft ? "-25vw" : "25vw",
+      }}
+      transition={{
+        bounce: 0,
+        duration: 2.5,
+        ease: "easeOut",
+        type: "spring",
+      }}
+      viewport={{ margin: "-20px", once: true }}
+      whileInView={{ opacity: 1, x: fromLeft ? "-110%" : "25%" }}
+    >
+      {children}
+    </motion.div>
+  );
 }
