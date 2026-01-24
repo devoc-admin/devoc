6a2f635f707c51f651846c0315089f144b514378
Thibaut Izard <thibaut.izard@gmail.com>
2026-01-24 15:28:45 +0100

    feat(web): add speed insight

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import {
   Dancing_Script,
@@ -87,6 +88,7 @@ export default function RootLayout({
           </main>
         </NuqsAdapter>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
