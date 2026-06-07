0e8a94960beecfe132979362cdbd4fa1ef0c8edc
devoc <internal.devoc@gmail.com>
2026-06-07 18:58:58 +0200

    style(web): improve contact card animation (#46)
    Co-authored-by: Thibaut Izard <thibaut.izard@gmail.com>

--- apps/web/app/(packs)/_components/sections/contact-card/contact-card-background.tsx
+++ apps/web/app/(packs)/_components/sections/contact-card/contact-card-background.tsx
@@ -3,26 +3,41 @@ import s from "./style.module.css";
 export function ContactCardBackground() {
   return (
     <div className={s.cardBackground}>
-      {/* 🟡 */}
-      <div
-        className={cn(
-          "absolute bottom-0 left-0",
-          "aspect-square h-[80%]",
-          "rounded-full",
-          "-translate-x-1/2 translate-y-1/2",
-          "bg-radial from-primary-strong to-80% to-transparent",
-          "blur-2xl"
-        )}
-      />
-      {/* 🟠 */}
+      {/* 🟠⬆️ */}
       <div
         className={cn(
           "absolute top-0 right-0",
-          "aspect-square h-full",
+          "aspect-square",
           "translate-x-1/4 -translate-y-1/4",
           "rounded-full",
           "bg-radial from-primary-strong to-80% to-transparent",
-          "blur-2xl"
+          "blur-2xl", // ↔️
+          "h-[40%]",
+          "xs:h-[40%]",
+          "sm:h-[50%]",
+          "md:h-[60%]",
+          "lg:h-[80%]",
+          "xl:h-[50%]",
+          "2xl:h-[50%]"
+        )}
+      />
+      {/* 🟠⬇️ */}
+      <div
+        className={cn(
+          "absolute bottom-0 left-0",
+          "rounded-full",
+          "-translate-x-1/2 translate-y-1/2",
+          "bg-radial from-primary-strong to-80% to-transparent",
+          "blur-2xl",
+          "aspect-square",
+          // ↔️
+          "h-[70%]",
+          "xs:h-[70%]",
+          "sm:h-[80%]",
+          "md:h-[80%]",
+          "lg:h-[80%]",
+          "xl:h-[80%]",
+          "2xl:h-[80%]"
         )}
       />
     </div>
