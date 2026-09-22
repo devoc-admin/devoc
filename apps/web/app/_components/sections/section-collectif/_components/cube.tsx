2c95ae8131c044053622845281477b2c81621bb6
Thibaut Izard <thibaut.izard@gmail.com>
2026-09-22 15:47:12 +0200

    perf(web): cube rendering only desktop

---
+++ apps/web/app/_components/sections/section-collectif/_components/cube.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { motion } from "motion/react";
+import dynamic from "next/dynamic";
+import { useCallback, useSyncExternalStore } from "react";
+import { cn } from "@/lib/utils";
+
+const RubiksCube = dynamic(
+  () => import("@/components/motion-core/rubiks-cube/rubiks-cube"),
+  { ssr: false }
+);
+
+export function CustomCube() {
+  const shouldDisplayCube = useMatchMedia("(width >= 64rem)");
+  return (
+    <motion.div
+      className={cn(
+        "mx-auto",
+        "max-lg:hidden",
+        "lg:mt-6 xl:mt-10 2xl:mt-14",
+        "lg:size-100 xl:size-110 2xl:size-120"
+      )}
+      initial={{
+        opacity: 0,
+      }}
+      transition={{
+        duration: 1,
+        ease: [0.7, 0, 0.84, 0],
+      }}
+      viewport={{ margin: "-100px", once: true }}
+      whileInView={{ opacity: 1 }}
+    >
+      {shouldDisplayCube && <RubiksCube />}
+    </motion.div>
+  );
+}
+
+function useMatchMedia(query: string) {
+  const subscribe = useCallback(
+    (onChange: () => void) => {
+      const media = window.matchMedia(query);
+      media.addEventListener("change", onChange);
+      return () => media.removeEventListener("change", onChange);
+    },
+    [query]
+  );
+  return useSyncExternalStore(
+    subscribe,
+    () => window.matchMedia(query).matches,
+    () => false
+  );
+}
