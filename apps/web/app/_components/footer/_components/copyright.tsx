95d997248efd57dea59a6a0613e8c4a8443a39ad
Thibaut Izard <thibaut.izard@gmail.com>
2026-09-23 21:10:41 +0200

    chore(web): reorder components

---
+++ apps/web/app/_components/footer/_components/copyright.tsx
@@ -0,0 +1,5 @@
+export function Copyright() {
+  return (
+    <div>&copy; {new Date().getFullYear()} Dev'Oc. Tous droits réservés.</div>
+  );
+}
---
+++ apps/web/app/_components/footer/_components/logo.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import Icon from "@/public/icon.svg";
+export function Logo() {
+  return (
+    <a
+      className={cn(
+        "group flex select-none items-center gap-3",
+        // ↔️
+        "text-[2rem]"
+      )}
+      href="#main-content"
+      rel="noopener noreferrer"
+    >
+      <Flower />
+      <div className="flex items-baseline gap-x-0.5">
+        <Dev />
+        <Oc />
+      </div>
+    </a>
+  );
+}
+
+// 🌼
+function Flower() {
+  return (
+    <Image
+      alt=""
+      aria-hidden="true"
+      className="-mt-0.5 transition-transform duration-1000 ease-in-out group-hover:rotate-360"
+      height={32}
+      src={Icon}
+      width={32}
+    />
+  );
+}
+
+function Dev() {
+  return (
+    <span className="font-black text-primary-foreground tracking-tighter">
+      Dev'
+    </span>
+  );
+}
+
+function Oc() {
+  return (
+    <span className="bg-linear-to-br from-[#FF5709] to-[#FFC731] bg-clip-text font-black font-regular text-transparent tracking-tighter">
+      Oc
+    </span>
+  );
+}
